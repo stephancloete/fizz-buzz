c1b4638e3cec1810003fe16394656cafc78466df
stephancloete <stephancloete@gmail.com>
2022-08-06 22:10:10 +0200

    Update index.js
    Third conditional added and tested

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-let x = 0;
+let x = 1;
 while (x <= 100){
     
-    
-    if (x % 3 == 0){
+    if (x % 3 == 0 && x % 5 == 0){
+        document.getElementById("update").innerHTML += "FizzBuzz" + "<br>";
+    }else if (x % 3 == 0){
         document.getElementById("update").innerHTML += "Fizz" + "<br>";
     }else if (x % 5 == 0){
         document.getElementById("update").innerHTML += "Buzz" + "<br>";
